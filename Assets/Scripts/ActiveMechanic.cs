2e247da8b336aeae7f19cd345b1464969c42c85e
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-08-03 08:55:37 +1000

    Created Active mechanic class

---
+++ Assets/Scripts/ActiveMechanic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CurrentMechanic {None, Gate, Teleporter, Blocks }
+
+public class ActiveMechanic : MonoBehaviour
+{
+    public static ActiveMechanic instance = null;
+    public CurrentMechanic currentMechanic;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
