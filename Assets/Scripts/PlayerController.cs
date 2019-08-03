796787dc91ff806e0fa67d92afbc5dc958e24362
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-08-03 22:30:30 +1000

    Created teleporter prefab, finished sliding block and player controller scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,28 +6,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 100;
+    public float pushTime = 0.1f;
     public LayerMask layerMask;
     public float boxCastOffset = 1;
     public Vector2 boxCastSize = new Vector2(1,1);
+
     private Vector2 direction;
     private Rigidbody2D playerRigidbody;
     private Vector2 boxCastOrigin;
 
     enum CurrentDirection {Down, Left, Up, Right}
     CurrentDirection currentDirection;
+    CurrentDirection directionWhenGrabbed;
+    private bool isGrabbing;
+    private bool canPushPull;
+    private float inverseMoveTime;
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
         playerRigidbody.gravityScale = 0;
+        inverseMoveTime = 1.0f / pushTime;
+        SlidingBlock.blockDropped += LetGoOfBlock;
     }
 
     void Update()
     {
-        Movement();
-        GetDirection();
-        RepositionBoxCast();
-        DetectInteractables();
+        if (isGrabbing)
+        {
+            PushAndPull();
+        }
+        else
+        {
+            Movement();
+            GetDirection();
+            RepositionBoxCast();
+            DetectInteractables();
+        }       
     }
 
     void OnTriggerEnter2D (Collider2D collider)
@@ -49,18 +64,22 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             currentDirection = CurrentDirection.Up;
+            // up facing animtion here
         }
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             currentDirection = CurrentDirection.Right;
+            // right facing animtion here
         }
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             currentDirection = CurrentDirection.Down;
+            // down facing animtion here
         }
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             currentDirection = CurrentDirection.Left;
+            // left facing animtion here
         }
     }
 
@@ -99,12 +118,93 @@ public class PlayerController : MonoBehaviour
             }
             else if (collider.GetComponent<SlidingBlock>())
             {
-                if (Input.GetKeyDown(KeyCode.E))
+                if (collider.gameObject.GetComponent<SlidingBlock>().IsMovable)
                 {
-                    collider.GetComponent<SlidingBlock>().PushBlock(boxCastOrigin / boxCastOffset);
-                }                
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        ResetVelocities();
+                        collider.GetComponent<SlidingBlock>().GrabBlock(transform);
+                        isGrabbing = true;
+                        directionWhenGrabbed = currentDirection;
+                        canPushPull = true;
+                    }
+                }                               
             }
         }
+    }
+
+
+    void PushAndPull ()
+    {
+        if (canPushPull)
+        {
+            if (directionWhenGrabbed == CurrentDirection.Left || directionWhenGrabbed == CurrentDirection.Right)
+            {
+                direction = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+                if (direction.x > 0)
+                {
+                    StartCoroutine(CoPushAndPull(transform.position + Vector3.right));
+                }
+                else if (direction.x < 0)
+                {
+                    StartCoroutine(CoPushAndPull(transform.position + Vector3.left));
+                }               
+            }
+            else if (directionWhenGrabbed == CurrentDirection.Up || directionWhenGrabbed == CurrentDirection.Down)
+            {
+                direction = new Vector2(0, Input.GetAxisRaw("Vertical"));
+                if (direction.y > 0)
+                {
+                    StartCoroutine(CoPushAndPull(transform.position + Vector3.up));
+                }
+                else if (direction.y < 0)
+                {
+                    StartCoroutine(CoPushAndPull(transform.position + Vector3.down));
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                LetGoOfBlock();
+            }
+        }        
+    }
+
+    IEnumerator CoPushAndPull (Vector3 end)
+    {
+        canPushPull = false;
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+        while (sqrRemainingDistance > 0.01f)
+        {
+            //Find a new position proportionally closer to the end, based on the moveTime
+            Vector3 newPostion = Vector3.MoveTowards(playerRigidbody.position, end, inverseMoveTime * Time.deltaTime);
+
+            //Call MovePosition on attached Rigidbody2D and move it to the calculated position.
+            playerRigidbody.MovePosition(newPostion);
+
+            //Recalculate the remaining distance after moving.
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+            //Return and loop until sqrRemainingDistance is close enough to zero to end the function
+            yield return null;
+        }
+        ResetVelocities();
+        
+        canPushPull = true;
+        print("ended coroutine");
+    }
+
+    void ResetVelocities ()
+    {
+        playerRigidbody.velocity = Vector3.zero;
+        playerRigidbody.angularVelocity = 0;
+    }
+
+    void LetGoOfBlock ()
+    {
+        isGrabbing = false;
+        transform.DetachChildren();
+        canPushPull = false;
+        StopAllCoroutines();       
     }
 
     void OnDrawGizmos ()
