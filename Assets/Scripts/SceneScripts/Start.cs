afa90072730ed132e95dfc1e0d9b3fce93a3b5a9
William Quintano <quintanw891@gmail.com>
2019-08-03 13:18:40 -0400

    Added Scene Manager and template Scenes
    Template Scenes for title, rules, credits, and win.

---
+++ Assets/Scripts/SceneScripts/Start.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        ActiveScene.CurrentScene = SceneType.Game;
+    }
+    
+}
