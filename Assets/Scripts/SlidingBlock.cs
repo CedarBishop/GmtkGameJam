2e0877ce7c539c642c7cea31eee71053c50e88df
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-08-03 19:36:23 +1000

    Created teleporter, sliding block scripts and finished active mechanic and player controller scripts

---
+++ Assets/Scripts/SlidingBlock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class SlidingBlock : MonoBehaviour
+{
+    private bool isMovable;
+    public bool IsMovable
+    {
+        get { return isMovable; }
+        set
+        {
+            isMovable = value;
+            rigidbody2D.isKinematic = (isMovable) ? false : true ;
+        }
+    }
+
+
+
+    private Rigidbody2D rigidbody2D;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.gravityScale = 0;
+        rigidbody2D.isKinematic = true;
+    }
+
+    public void PushBlock (Vector2 currentPlayerDirection)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Teleporter : MonoBehaviour
+{
+    public Teleporter partnerTeleporter;
+    public Color activatedColor;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isActivated;
+    public bool IsActivated
+    {
+        get { return isActivated; }
+        set
+        {
+            isActivated = value;
+            spriteRenderer.color = (isActivated) ? activatedColor : Color.white;
+        }
+    }
+
+    void OnValidate()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    public void TeleportPlayer (GameObject player)
+    {
+        player.transform.position = partnerTeleporter.transform.position + Vector3.right;
+    }
+}
