70c675fc3001badf481f7959d7b984c09a1cf6ea
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-08-04 12:56:42 +1000

    Added camera follow and colored all switches and teleporters

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform playerTransform;
+
+    void Start()
+    {
+        playerTransform = FindObjectOfType<PlayerController>().transform;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x,playerTransform.position.y, transform.position.z);
+    }
+}
